957007966ff74a037afbcbb39b3c0a5b528832eb
Brian Curless <bcurl3ss@gmail.com>
2023-08-24 18:34:29 -0700

    Created python gitignore and stubbed out a script

---
+++ nestLogger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Simple script to acquire temperature from nest thermostat
+"""
+
+import os
+import sys
+
+
+def main(arguments):
+
+    print(arguments)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
