cecff2f0569235ef32d6d251aceba21c9e041774
Brian Curless <bcurl3ss@gmail.com>
2023-09-16 21:34:49 -0700

    Removed whole house fan temporarily while I test

--- tempLogger.py
+++ tempLogger.py
@@ -2,6 +2,7 @@
 
 """Simple script to acquire and log
 temperature from nest thermostat
+as well as the state of my whole house fan
 """
 
 import os
@@ -10,19 +11,29 @@ import time
 
 from thermostat import Thermostat
 from database import Database
+from kasaoutlet import KasaOutlet
 
 
 def main():
     nestThermostatTable = "nestThermostat"
+    wholeHouseFanTable = "wholeHouseFan"
     nestdb = "nestdb.db"
-    nest = Thermostat()
-    device = nest.get_devices()[0]
-    device_name = device["name"]  # .split('/')[-1]
+    whf = "Whole House Fan"
+
     db = Database(nestdb)
     db.create_data_table(nestThermostatTable)
+    db.create_data_table(wholeHouseFanTable)
 
+    nest = Thermostat()
+    device = nest.get_devices()[0]
+    device_name = device["name"]
     currentTemp = nest.get_temp(device_name)
     db.add_data(nestThermostatTable, currentTemp)
+
+    # outlet = KasaOutlet()
+    # fan = outlet.discover_device(whf)
+    # state = outlet.get_state(fan)
+    # db.add_data(wholeHouseFanTable, state)
 
 
 if __name__ == "__main__":
