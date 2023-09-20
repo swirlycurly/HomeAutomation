1db72f9e4af922439749dc3ca2000360abba3a63
Brian Curless <bcurl3ss@gmail.com>
2023-09-19 21:59:22 -0700

    Changed weather module to use closure to manage local variable and return a forecast getter. This way we don't need to repeat the api call to resolve the correct grid points from a given latitude and longitude

--- weather/weather.py
+++ weather/weather.py
@@ -1,6 +1,7 @@
 import logging
 import requests
 from requests.compat import urljoin
+import json
 
 
 logger = logging.getLogger(__name__)
@@ -9,19 +10,25 @@ flagCoords = (35.1727, -111.6795)
 timeout = 5
 
 
-def get_forecast(lat, long):
+def get_forecast_reader(lat, long):
     try:
         headers = {"user-agent": "bcurl3ss@gmail.com"}
         url = urljoin(base, f"/points/{lat},{long}")
         r = requests.get(url, headers=headers, timeout=timeout)
         r.raise_for_status()
         fcastend = r.json()["properties"]["forecastHourly"]
-        r = requests.get(fcastend, headers=headers, timeout=timeout)
-        return r.json()
+
+        def get_forecast():
+            r = requests.get(fcastend, headers=headers, timeout=timeout)
+            return r.json()
+
+        return get_forecast
 
     except Exception as e:
         logger.error("Failed to get weather forecast", e)
+        raise e
 
 
 if __name__ == "__main__":
-    print(get_forecast(*flagCoords))
+    get_forecast = get_forecast_reader(*flagCoords)
+    print(json.dumps(get_forecast(), indent=2))
